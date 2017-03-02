850088b00687558de39d49d9571aa3cd06392e83
Daniel Pelsmaeker <info@virtlink.com>
2017-03-02 10:38:00 +0100

    Add shift register for outputs

--- arduino/pinball/pinball.ino
+++ arduino/pinball/pinball.ino
@@ -1,9 +1,9 @@
 /*15ms max 1 per sec*/
 
 
-const int SIGPINL = 4;
+const int SIGPINL = 0; //shift
 const int BUTTONPINL = 2;
-const int SIGPINR = 5;
+const int SIGPINR = 1; //shift
 const int BUTTONPINR = 3;
 
 int buttonStateL = 0;
@@ -13,9 +13,13 @@ const int SLING_IDLE = 0;
 const int SLING_TRIGGERED_HIGH = 1;
 const int SLING_TRIGGERED_LOW = 2;
 
+const int PIN_LATCH = 8;
+const int PIN_CLOCK = 12;
+const int PIN_DATA = 11;
+
 const int numSlings = 3;
-int slingTrigger[numSlings] = {6, 7, 10};
-int slingFire[numSlings]    = {8, 9, 11};
+int slingTrigger[numSlings] = {4, 5, 6};
+int slingFire[numSlings]    = {2, 3, 4}; //shift
 int slingState[numSlings];
 unsigned long lastTriggered[numSlings];
 
@@ -24,8 +28,6 @@ void initSling(){
     slingState[i] = SLING_IDLE;
     lastTriggered[i] = 0;
     pinMode(slingTrigger[i], INPUT);
-    pinMode(slingFire[i], OUTPUT);
-    digitalWrite(slingFire[i], LOW);
   }
 }
 
@@ -41,11 +43,11 @@ void slingShot(int n, int interval, int highLength) {
   if(slingState[n] == SLING_IDLE && digitalRead(slingTrigger[n])) {
     slingState[n] = SLING_TRIGGERED_HIGH;
     lastTriggered[n] = currentTime;
-    digitalWrite(slingFire[n], HIGH);
+    shiftWrite(slingFire[n], HIGH);
   } else if(slingState[n] == SLING_TRIGGERED_HIGH &&
            currentTime - lastTriggered[n] > highLength) {
     slingState[n] = SLING_TRIGGERED_LOW;
-    digitalWrite(slingFire[n], LOW); 
+    shiftWrite(slingFire[n], LOW); 
   } else if(slingState[n] == SLING_TRIGGERED_LOW &&
             currentTime - lastTriggered[n] > interval) {
     slingState[n] = SLING_IDLE;
@@ -60,9 +62,9 @@ const int PLUNGE_HIGH = 4;
 
 int plungerState = PLUNGE_IDLE;
 unsigned long plungerTimeStamp;
-int plungeTrigger = 10;
-int troughFire = 11;
-int plungeFire = 12;
+int plungeTrigger = 7;
+int troughFire = 5; //shift
+int plungeFire = 6; //shift
 void plunger(int drainSettleTime, int troughHighTime, int moveToLaneTime, int plungeHighTime) {
   unsigned long currentTime = millis();
   if(plungerState == PLUNGE_IDLE && digitalRead(plungeTrigger)) {
@@ -72,40 +74,37 @@ void plunger(int drainSettleTime, int troughHighTime, int moveToLaneTime, int pl
             currentTime - plungerTimeStamp > drainSettleTime) {
     plungerState = PLUNGE_TROUGH_HIGH;
     plungerTimeStamp = currentTime;
-    digitalWrite(troughFire, HIGH);
+    shiftWrite(troughFire, HIGH);
   } else if(plungerState == PLUNGE_TROUGH_HIGH && 
             currentTime - plungerTimeStamp > troughHighTime) {
     plungerState = PLUNGE_TROUGH_LOW;
     plungerTimeStamp = currentTime;
-    digitalWrite(troughFire, LOW);          
+    shiftWrite(troughFire, LOW);          
   } else if(plungerState == PLUNGE_TROUGH_LOW &&
             currentTime - plungerTimeStamp > moveToLaneTime) {
     plungerState = PLUNGE_HIGH;
     plungerTimeStamp = currentTime;
-    digitalWrite(plungeFire, HIGH);
+    shiftWrite(plungeFire, HIGH);
   } else if(plungerState == PLUNGE_HIGH &&
             currentTime - plungerTimeStamp > plungeHighTime) {
     plungerState = PLUNGE_IDLE;
-    digitalWrite(plungeFire, LOW);          
+    shiftWrite(plungeFire, LOW);          
   }
 }
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   pinMode(BUTTONPINL, INPUT);
-  pinMode(SIGPINL, OUTPUT);
   pinMode(BUTTONPINR, INPUT);
-  pinMode(SIGPINR, OUTPUT);
+  
+  pinMode(PIN_LATCH,  OUTPUT);
+  pinMode(PIN_CLOCK,  OUTPUT);
+  pinMode(PIN_DATA,   OUTPUT);
   
   initSling();
-  /*
+  
   pinMode(plungeTrigger, INPUT);
-  pinMode(troughFire, OUTPUT);
-  pinMode(plungeFire, OUTPUT);
-  digitalWrite(troughFire, LOW);
-  digitalWrite(plungeFire, LOW);
-*/
-
+  
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
@@ -123,15 +122,15 @@ void loop() {
   // check if the pushbutton is pressed.
   // if it is, the buttonState is HIGH:
   if (buttonStateL == HIGH) {
-    digitalWrite(SIGPINL, HIGH);   // turn the LED on (HIGH is the voltage level)
+    shiftWrite(SIGPINL, HIGH);   // turn the LED on (HIGH is the voltage level)
   } else {
-    digitalWrite(SIGPINL, LOW);    // turn the LED off by making the voltage LOW
+    shiftWrite(SIGPINL, LOW);    // turn the LED off by making the voltage LOW
   }
 
   if (buttonStateR == HIGH) {
-    digitalWrite(SIGPINR, HIGH);
+    shiftWrite(SIGPINR, HIGH);
   } else {
-    digitalWrite(SIGPINR, LOW);
+    shiftWrite(SIGPINR, LOW);
   }
 
   bool ledLit = buttonStateL == HIGH || buttonStateR == HIGH || plungerState == PLUNGE_TROUGH_HIGH || plungerState == PLUNGE_HIGH;
@@ -143,4 +142,18 @@ void loop() {
   else{
     digitalWrite(LED_BUILTIN, LOW);
   }
+  shiftCommit();
 }
+
+
+byte shiftOutput = 0;
+
+void shiftWrite(int pin, int state) {
+  bitWrite(shiftOutput, pin, state);
+}
+void shiftCommit(){
+  digitalWrite(PIN_LATCH, LOW);
+  shiftOut(PIN_DATA, PIN_CLOCK, MSBFIRST, shiftOutput);
+  digitalWrite(PIN_LATCH, HIGH);
+}
+
